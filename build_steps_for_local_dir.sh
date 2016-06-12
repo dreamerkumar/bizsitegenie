1f6b4bdf34638a1a6b948fc51f638c9d8ab8c9a8
Vishal-Kumar <kumarfromatlanta@hotmail.com>
2016-06-12 13:57:37 -0400

    build steps that affect the local folder

---
+++ build_steps_for_local_dir.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# redirect stdout/stderr to a file
+# exec &> logfile.txt
+exec > >(tee -i build_steps_for_local_dir_results.log)
+exec 2>&1
+echo "START ---> build_steps_for_local_dir"
+echo "COMMAND ---> cd website"
+cd website
+echo "COMMAND ---> npm install"
+npm install
+echo "COMMAND ---> bower install"
+bower install
+echo "COMMAND ---> grunt build"
+grunt build
+echo "COMMAND ---> cd public"
+cd public
+echo "COMMAND ---> 777 modules"
+chmod 777 modules
+echo "COMMAND ---> ../app"
+cd ../app
+echo "COMMAND ---> 777 *"
+chmod 777 *
+echo "COMMAND ---> mkdir -p models"
+mkdir -p models
+echo "COMMAND ---> 777 models"
+chmod 777 models
+echo "COMMAND ---> ../../generator-builder"
+cd ../../generator-builder
+echo "COMMAND ---> npm install"
+npm install
+echo "END ---> build_steps_for_local_dir"
