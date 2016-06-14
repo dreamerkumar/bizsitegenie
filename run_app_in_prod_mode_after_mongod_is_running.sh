b69183951a4751bb2453a2b3b8396f724f82617d
Vishal-Kumar <kumarfromatlanta@hotmail.com>
2016-06-14 00:21:42 -0400

    fixed issues with last script

--- run_app_in_prod_mode_after_mongod_is_running.sh
+++ run_app_in_prod_mode_after_mongod_is_running.sh
@@ -3,6 +3,8 @@
 exec > >(tee -i run_app_in_prod_mode_after_mongod_is_running_results.log)
 exec 2>&1
 echo "START ---> run_app_in_prod_mode_after_mongod_is_running"
-echo "COMMAND ---> NODE_ENV=production PORT=80 forever website/server.js"
-NODE_ENV=production PORT=80 forever website/server.js
+echo "COMMAND ---> cd website"
+cd website
+echo "COMMAND ---> NODE_ENV=production forever server.js"
+NODE_ENV=production forever server.js
 echo "END ---> run_app_in_prod_mode_after_mongod_is_running"
