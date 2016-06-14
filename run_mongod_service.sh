b1619b9486a96b947a8e3917508b93064c5481ce
Vishal-Kumar <kumarfromatlanta@hotmail.com>
2016-06-14 01:04:05 -0400

    more script files to run the application

---
+++ run_mongod_service.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# redirect stdout/stderr to a file and console
+exec > >(tee -i run_mongod_service_results.log)
+exec 2>&1
+echo "START ---> run_mongod_service"
+echo "COMMAND ---> mkdir -p mongodb"
+mkdir -p mongodb
+echo "COMMAND ---> mongod --dbpath mongodb"
+mongod --dbpath mongodb
+echo "END ---> run_mongod_service"
