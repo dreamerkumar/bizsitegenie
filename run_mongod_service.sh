235529c85330fac7392c19ee50dff271d209a899
Vishal-Kumar <kumarfromatlanta@hotmail.com>
2016-06-18 15:54:13 -0400

    assign permissions to the mongodb folder

--- run_mongod_service.sh
+++ run_mongod_service.sh
@@ -5,6 +5,8 @@ exec 2>&1
 echo "START ---> run_mongod_service"
 echo "COMMAND ---> mkdir -p mongodb"
 mkdir -p mongodb
+echo "COMMAND ---> chmod 777 mongodb"
+chmod 777 mongodb
 echo "COMMAND ---> mongod --dbpath mongodb"
 mongod --fork --logpath mongodb.log --dbpath mongodb 
 echo "END ---> run_mongod_service"
